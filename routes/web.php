d6a17103ab78a525ee9ffbdc0f7eb3bdec5aa5c2
azamat-li <azamatalifullstack@gmail.com>
2020-12-14 17:34:25 +0500

    test: ttd-ed projects.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () { return view('welcome'); });
+
+// Projects
+
+Route::get('/projects', function () {
+    $projects = App\Models\Project::all();
+
+    return view('projects.main',
+        compact('projects'));
 });
+
+Route::post('/projects', function ()
+{
+    // validate
+
+    App\Models\Project::create(request(['title', 'description']));
+});
+
